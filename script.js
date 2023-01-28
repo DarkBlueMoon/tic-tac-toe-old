504167b793bd5b7d87ead0e71f2cb3a7e5402d86
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2023-01-27 20:35:36 -0600

    Grid setup (HTML), grid styling

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello world!");
+function PlayerFactory(name, mark) {
+  return { name, mark };
+}
+
+const gameBoard = (() => {
+  const board = new Array(9).fill(null);
+
+  // Func: render board to DOM container
+  // Func: check for winner after each click.
+
+  return { board };
+})();
+
+// Root of the game.
+const gameManager = (() => {
+  // Create the players & hold reference to the two players.
+  const player1 = PlayerFactory("Tom", "X");
+  const player2 = PlayerFactory("John", "O");
+
+  // Create the gameBoard and hold reference to the gameBoard.
+
+  // Func: switch player upon valid move
+  // Func: choose first player upon game start
+  return { player1, player2 };
+})();
