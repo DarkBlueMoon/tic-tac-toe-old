8859abf43f4ac5dbc2c3f4552af2ab405e6cf13b
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2023-01-31 14:10:50 -0600

    Board styling, player can add mark to screen

--- script.js
+++ script.js
@@ -2,24 +2,14 @@ function PlayerFactory(name, mark) {
   return { name, mark };
 }
 
-// const boardContainer = document.querySelector(".container");
-// boardContainer.addEventListener("click", (ev) => {
-//   console.dir(ev.target);
-//   console.log(Array.from(ev.target.parentNode.children).indexOf(ev.target));
-// });
-
 const gameBoard = (() => {
-  // const boardDOMCells = boardContainer.children;
-  const board = new Array(9).fill(null);
-  board[0] = "X";
-  board[4] = "X";
-  board[8] = "X";
+  const _boardDOMCells = document.querySelector(".container").children;
+  const _board = new Array(9).fill(null);
 
-  // Func: render board to DOM container
   function render() {
-    for (let i = 0; i < board.length; i++) {
-      if (board[i] !== null) {
-        // boardDOMCells[i].textContent = board[i];
+    for (let i = 0; i < _board.length; i++) {
+      if (_board[i] !== null) {
+        _boardDOMCells[i].textContent = _board[i];
       }
     }
   }
@@ -29,23 +19,21 @@ const gameBoard = (() => {
   function addMark(clickEvent) {
     const boardIndex = clickEvent.target.getAttribute("data-cell-index");
 
-    if (board[boardIndex] === null) {
-      board[boardIndex] = gameManager.getCurrentPlayer().mark;
+    if (_board[boardIndex] === null) {
+      _board[boardIndex] = gameManager.getCurrentPlayer().mark;
+      gameManager.changeCurrentPlayer();
     }
-
+    render();
     checkForWinner();
   }
-  // Func: check for winner after each click.
 
-  return { board, render, addMark };
+  return { render, addMark };
 })();
 
-// Root of the game.
 const gameManager = (() => {
-  // Create the players & hold reference to the two players.
-  const player1 = PlayerFactory("Tom", "X");
-  const player2 = PlayerFactory("John", "O");
-  let currentPlayer = player1;
+  const _player1 = PlayerFactory("Tom", "X");
+  const _player2 = PlayerFactory("John", "O");
+  let _currentPlayer = _player1;
 
   function setupEventListeners() {
     document
@@ -54,19 +42,15 @@ const gameManager = (() => {
   }
 
   function getCurrentPlayer() {
-    return currentPlayer;
+    return _currentPlayer;
   }
 
   function changeCurrentPlayer() {
-    getCurrentPlayer() === player1
-      ? (currentPlayer = player2)
-      : (currentPlayer = player1);
+    getCurrentPlayer() === _player1
+      ? (_currentPlayer = _player2)
+      : (_currentPlayer = _player1);
   }
 
   setupEventListeners();
-  // Create the gameBoard and hold reference to the gameBoard.
-
-  // Func: switch player upon valid move
-  // Func: choose first player upon game start
   return { getCurrentPlayer, changeCurrentPlayer };
 })();
